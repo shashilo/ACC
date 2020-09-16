3735c8696e8de4330a865cf640a94f580ec45a79
Shashi Lo <shlo@adobe.com>
2020-09-15 20:58:51 -0500

    Adding unit testing and cleaning up code

--- src/Components/Results/Results.tsx
+++ src/Components/Results/Results.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
+import React, { Key } from 'react'
 import { ResultsInterface } from '../Pages/Home/HomeInterface';
 import './Results.scss';
 
 export const Results: React.FC<any> = ({ results }) => {
     return (
-        <>
-            { results.map((item: ResultsInterface) => {
+        <div className="results-content">
+            { results.map((item: ResultsInterface, index: Key) => {
                 return([
-                    <div className="results-content__item">
-                        <img src={ item.image } alt="" />
-                        <h2 className="-mt-25">{ item.title }</h2>
-                        <p>{ item.description }</p>
-                        <a href={ item.link } target="_blank" rel="noopener noreferrer">Learn more</a>
+                    <div className="results-content__item" key={ index }>
+                        <img src={ item.image } alt="" data-testid="image" />
+                        <h2 className="-mt-25" data-testid="title">{ item.title }</h2>
+                        <p data-testid="description">{ item.description }</p>
+                        <a href={ item.link } target="_blank" rel="noopener noreferrer" data-testid="link">Learn more</a>
                     </div>
                 ])
-            }
-            )}
-        </>
+            })}
+        </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import 'normalize.css';
 import './shared/styles/main.scss';
 import { Routes } from './routes';
 import { Header } from './Components/Layout/Header/Header';
